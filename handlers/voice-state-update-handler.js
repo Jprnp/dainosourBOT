346ae2f6eb3964869d20ffeb3ada18d005135dc2
João Pedro Pinheiro <joao.pinheiro@zgsolucoes.com.br>
2021-10-29 09:08:06 -0300

    Identificar os mininu

--- handlers/voice-state-update-handler.js
+++ handlers/voice-state-update-handler.js
@@ -9,25 +9,25 @@ module.exports = {
         if (!oldState.channel && newState.channel) {
             const audioController = AudioController.getInstance(newState.channel);
             const nickname = newState.member.nickname || newState.member.user.username;
-            switch(newState.member.user.id) {
-                case '180302316145082368':
+            switch (newState.member.user.id) {
+                case '180302316145082368':                                                  // MINI
                     audioController.playTrack(dinoFilePath, dinoVolume);
                     channel.send(`${nickname} entrou e tá de sacanagem`)
                     break;
-                case '243070073349210112':
+                case '243070073349210112':                                                  // HYAGO
                     audioController.playTrack(tzDaCoro, tzVolume);
                     channel.send(`${nickname} entrou com um peixe no bolso`)
                     break;
-                case '277210762613751808':
+                case '277210762613751808':                                                  // PEDRO
                     audioController.playTrack(dinoFilePath, dinoVolume);
                     channel.send(`${nickname} entrou e tá de sacanagem`)
                     break;
-                case '465601161098166272':
+                case '465601161098166272':                                                  // PP
                     audioController.playTrack(yameteFilePath, yameteVolume);
                     channel.send(`${nickname} saiu da chamada`)
                     break;
             }
         }
-        
+
     }
 }
